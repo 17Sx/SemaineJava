99560f4327996651d1d53d3c3bffaffc87011b1d
17Sx <145352018+17Sx@users.noreply.github.com>
2023-12-26 18:44:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -49,14 +49,16 @@ document.addEventListener('DOMContentLoaded', function() {
 let passwordInput = document.getElementById('password');
 function validatePassword() {
     let passwordError = document.getElementById('mdpError');
-    let passwordRegex = /^(?=.[A-Z])(?=.[a-z])(?=.[!@#$%^&()_+{}[]:;<>,.?~\/-]).{8,}$/;
+    let passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&()_+{}[\]:;<>,.?~\/-]).{8,}$/;
 
-    if (passwordRegex.test(passwordInput.value) && passwordInput.value.length >= 8) {
-        passwordError.textContent = ''; 
+    if (!(passwordRegex.test(passwordInput.value) && passwordInput.value.length >= 8)) {
         passwordError.textContent = 'Le mot de passe doit contenir au moins une majuscule, une minuscule et un caractère spécial, et faire au moins 8 caractères de long.';
+    } else {
+        passwordError.textContent = ''; 
     }
 }
-passwordInput.addEventListener('input',validatePassword)
+passwordInput.addEventListener('input', validatePassword);
+
 
 
 function appmenu() {
